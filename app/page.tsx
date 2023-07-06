d1e55e88609f6c7044928f2acefd41e382dbceb6
James <jamesdenoy56@gmail.com>
2023-07-06 10:29:03 +0800

    initial commit of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,22 @@
 import Image from 'next/image'
-import doublechevron from './Images/doublechevron.png'
-import todayicon from './Images/todayicon.png'
-import calendaricon from './Images/calendaricon.png'
-import stickyicon from './Images/stickyicon.png'
+import icon from './Images/icon.png'
+import homebg from './Images/test.jpg'
 export default function Home() {
   return (
-  <div className=' ml-4 float-left h-screen p-10 rounded-xl bg-gray-300'>
-    <h2 className='font-bold mb-10 text-center text-2xl'>Menu</h2>
- <div className='flex flex-col justify-center items-center gap-8'>
-  <p className='mr-auto'>Tasks</p>
-  <a href='/' className='flex flex-row items-center justify-center gap-4' >
+ <div>
+<div className='h-screen homepage w-screen flex flex-col justify-center items-center  object-cover bg-black'>
 
-    
-    <Image 
-    className='h-[1.5rem] w-[1.5rem] object-contain'
-    src={doublechevron}
-    alt='UPCOMING icon'
-
-    />
-
- <p className='w-[6.5rem]' >Upcoming</p>
+  <Image
+  className='object-cover'
+  src={icon}
 
 
-<span className='text-[0.813rem] rounded-full w-[2rem] text-center bg-gray-400 px-2 py-1'>21</span>
-
-</a>
+  alt='ICON'
   
-<a href='/' className='flex flex-row items-center justify-center gap-4' >
-
-    
-<Image 
-className='h-[1.5rem] w-[1.5rem] object-contain'
-src={todayicon}
-alt='UPCOMING icon'
-
-/>
-
-<p className='w-[6.5rem]'>Today</p>
+  />
 
 
-<span className='text-[0.813rem] rounded-full w-[2rem] text-center bg-gray-400 px-2 py-1'>21</span>
-
-</a>
-<a href='/' className='flex flex-row items-center justify-center gap-4' >
-
-    
-<Image 
-className='h-[1.5rem] w-[1.5rem] object-contain'
-src={calendaricon}
-alt='UPCOMING icon'
-
-/>
-
-<p className='w-[6.5rem]'>Calendar</p>
-
-
-<span className='text-[0.813rem] rounded-full w-[2rem] text-center bg-gray-400 px-2 py-1 '>21</span>
-
-</a>
-
-<a href='/' className='flex flex-row items-center justify-center gap-4' >
-
-    
-<Image 
-className='h-[1.7rem] w-[1.7rem] object-contain'
-src={stickyicon}
-alt='UPCOMING icon'
-
-/>
-
-<p className='w-[6.5rem]'>Sticky Wall</p>
-
-
-<span className='text-[0.813rem] rounded-full w-[2rem] text-center bg-gray-400 px-2 py-1 '>1</span>
-
-</a>
+</div>
  </div>
-  </div>
   )
 }
