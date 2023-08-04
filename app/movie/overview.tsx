3312b768722222e7064c6fba2e9c15ce6a8119dd
James <jamesdenoy56@gmail.com>
2023-08-04 11:04:15 +0800

    sadaa

--- app/movie/overview.tsx
+++ app/movie/overview.tsx
@@ -216,7 +216,7 @@ const separtedNames = genreNames && genreNames.join( ' ' + '•' + ' ')
           />
     
       <Image
-         className='mr-1  h-[0.9rem] w-[0.9rem] md:h-[1.1rem] md:w-[1.1rem] 2xl:h-[1.3rem] 2xl:w-[1.3rem] object-contain'
+         className='mr-1  h-[0.85rem] w-[0.85rem] md:h-[1rem] md:w-[1rem] 2xl:h-[1.2rem] 2xl:w-[1.2rem] object-contain'
          src={star}
          alt='home icon'
          width={1}
@@ -227,7 +227,7 @@ const separtedNames = genreNames && genreNames.join( ' ' + '•' + ' ')
           {movieDetails.vote_average
           
           ?
-          <p className='flex self-center mr-4 text-[0.85rem] md:text-[1rem] 2xl:text-[1.2rem]'>{movieDetails.vote_average && movieDetails.vote_average.toFixed(1)}</p>
+          <p className='mt-[2px] sm:mt-[.5px] mr-4 text-[0.85rem] md:text-[1rem] 2xl:text-[1.2rem] '>{movieDetails.vote_average && movieDetails.vote_average.toFixed(1)}</p>
           :
           <p className='mr-4 text-[0.85rem] md:text-[1rem] 2xl:text-[1.2rem]'>N/A</p>
           }
