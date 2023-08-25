38f73801133fcdac1b577e4af169e3cea44232ca
James <jamesdenoy56@gmail.com>
2023-08-25 22:37:56 +0800

    adjusted close button on random trailers

--- app/movie/[id]/Random-Trailer_Modal.tsx
+++ app/movie/[id]/Random-Trailer_Modal.tsx
@@ -37,9 +37,9 @@ const Modal :  React.FC<ModalProps>  =   ({ isVisible, onClose, MovieData }) => 
   
       {isVisible  && movieVid !== null ? (
         <div className='h-screen w-full bg-opacity-25 z-[100000] backdrop-blur-sm fixed mx-auto flex justify-center items-center'>
-          <div className='flex items-center justify-center relative mx-4 sm:mx-0 h-[50%] w-full sm:w-[80%] sm:h-[55%] md:w-[70%] md:h-[60%] lg:w-[55%] lg:h-[65%] overflow-x-hidden '>
+          <div className='flex items-center justify-center relative mx-4 sm:mx-0 h-[50%] w-full sm:w-[80%] sm:h-[55%] md:w-[70%] md:h-[60%] lg:w-[55%] lg:h-[65%]  '>
             <button
-              className='absolute top-0 text-[0.75rem] rounded-full bg-gray-800 w-fit h-fit px-2 py-[2px] right-0 z-[9999999] mx-auto'
+              className='absolute top-[-30px] text-[0.80rem] md:text-[0.95rem] rounded-full bg-[#1a1a1a] w-fit h-fit px-2  pb-[1px] right-0 z-[9999999] mx-auto'
               onClick={onClose}
             >
               ✕
